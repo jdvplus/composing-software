8ae9f982ff705163e67c723fb1e733620ccf562a
Jaime de Venecia <jaime@augihealth.com>
2026-07-03 00:22:46 -0700

    feat: implement flip util

---
+++ src/flip.js
@@ -0,0 +1,15 @@
+/**
+ * Takes a function, and returns a function which takes a parameter `a`, and
+ * returns a function which takes a parameter `b`, which returns the result of
+ * executing the function returned by invoking the function passing in `b`,
+ * passing in `a`.
+ *
+ * In short, this utility function swaps the order of parameters in a curried
+ * function.
+ */
+export const flip = (fn) => (a) => (b) => fn(b)(a)
+
+import { trace } from './trace.js'
+const flippedTrace = flip(trace)
+const flipped = flippedTrace('hi')
+flipped('there') // 'there: hi'
