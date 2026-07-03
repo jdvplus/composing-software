b3de16ad7a9bf4180ac2407a1852a5d503dc130b
Jaime de Venecia <jaime@augihealth.com>
2026-07-03 01:15:50 -0700

    chore(pipe): move pseudo-code

--- src/utils/pipe.js
+++ src/utils/pipe.js
@@ -2,9 +2,9 @@
  * Takes any number of functions, and returns a function which takes an initial
  * value `x` and uses `reduce()` to iterate L-R over every function `f` in `fns`
  * and apply it to the accumulated value `y`.
+ *
+ * Essentially, composes in reverse order.
  */
 export const pipe = (...fns) => {
   return (x) => fns.reduce((y, f) => f(y), x)
 }
-
-// (composes in reverse order)
