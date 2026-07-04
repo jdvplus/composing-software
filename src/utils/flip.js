333b44fce8ed86670c384dc870b2155ccc6cf47b
Jaime de Venecia <jaime@augihealth.com>
2026-07-04 11:07:57 -0700

    update(flip): add unflipped test

--- src/utils/flip.js
+++ src/utils/flip.js
@@ -10,6 +10,10 @@
 export const flip = (fn) => (a) => (b) => fn(b)(a)
 
 import { trace } from './trace.js'
+
+const unflipped = trace('hi')
+unflipped('there') // 'hi: there'
+
 const flippedTrace = flip(trace)
 const flipped = flippedTrace('hi')
 flipped('there') // 'there: hi'
