22ba167b8225338aed062734137a2e73a281f4f2
Jaime de Venecia <jaime@augihealth.com>
2026-07-04 00:15:20 -0700

    test(stack): write axiom tests for stack implementation

---
+++ src/abstract-data-types/stack.test.ts
@@ -0,0 +1,32 @@
+import { assert } from '../test/assert.ts'
+import { stack_adt, push_adt, pop_adt, stack, push, pop } from './stack.ts'
+
+assert({
+  given: 'push `a` to the stack and immediately pop from the stack',
+  should: 'return a pair of `a` and `stack()`',
+  actual: pop_adt(push_adt('a', stack_adt())),
+  expected: ['a', stack_adt()],
+})
+
+assert({
+  given:
+    'push `a` to the stack, then push `b` to the stack, then pop from the stack',
+  should: 'return a pair of `b` and `stack(a)`.',
+  actual: pop_adt(push_adt('b', push_adt('a', stack_adt()))),
+  expected: ['b', stack_adt('a')],
+})
+
+assert({
+  given: 'push `a` to the stack and immediately pop from the stack',
+  should: 'return a pair of `a` and `stack()`',
+  actual: pop(push('a', stack())),
+  expected: ['a', stack()],
+})
+
+assert({
+  given:
+    'push `a` to the stack, then push `b` to the stack, then pop from the stack',
+  should: 'return a pair of `b` and `stack(a)`.',
+  actual: pop(push('b', push('a', stack()))),
+  expected: ['b', stack('a')],
+})
