222c9cda6506cee75a5407e8a07def1984f12c4e
Jaime de Venecia <jaime@augihealth.com>
2026-07-03 23:32:07 -0700

    refactor(stack): rename types/methods, add 'print' implementation, update pop testing for both tests

--- src/abstract-data-types/stack.ts
+++ src/abstract-data-types/stack.ts
@@ -1,17 +1,19 @@
 // implementing w/ an encapsulated object + pure functions
-type Stack<T> = {
-  push: (item: T) => Stack<T>
-  pop: () => [T | undefined, Stack<T>]
-  toString: () => string
+type StackADTObject<T> = {
+  push: (item: T) => StackADTObject<T>
+  pop: () => [T | undefined, StackADTObject<T>]
+  print: () => T[]
+  printString: () => string
 }
-type StackADT<T> = (...items: T[]) => Stack<T>
+type StackADT<T> = (...items: T[]) => StackADTObject<T>
 
 /**
  * Takes an arbitrary number of items, and returns an object with three methods:
  *
  * 1. `push` - Takes an item and pushes it into the Stack.
- * 2. `pop` - Returns a tuple of the popped item and the updated Stack.;
- * 3. `toString` - Returns a string representing the current Stack.
+ * 2. `pop` - Returns a tuple of the popped item and the updated Stack.
+ * 3. `print` - Returns the current Stack.
+ * 4. `printString` - Returns a string representing the current Stack.
  *
  * Key takeaway: the Stack itself lives in the `items` array, captured by the
  * closure of each returned object. Each time `stack(...)` is called, we get a
@@ -19,52 +21,70 @@ type StackADT<T> = (...items: T[]) => Stack<T>
  * Stack is recreated from scratch on every operation, and thus every instance
  * is _immutable_.
  */
-const stack: StackADT<unknown> = (...items) => ({
-  push: (item) => stack(...items, item),
+const stack_adt: StackADT<unknown> = (...items) => ({
+  push: (item) => stack_adt(...items, item),
 
   pop: () => {
-    const newItems = [...items]
+    const newItems = [...items] // can use items.slice() too
     const [item] = newItems.splice(-1)
 
-    return [item, stack(...newItems)]
+    return [item, stack_adt(...newItems)]
   },
 
-  toString: () => `Stack: [${items.join(', ')}]`,
+  print: () => [...items],
+
+  printString: () => `[${items.join(', ')}]`,
 })
 
-const push = (item: unknown, s: Stack<unknown>) => s.push(item)
-const pop = (s: Stack<unknown>) => s.pop()
+const push_adt = (item: unknown, s: StackADTObject<unknown>) => s.push(item)
+const pop_adt = (s: StackADTObject<unknown>) => s.pop()
 
-let s1 = stack()
+console.log('--- ADT tests ---')
+let s1 = stack_adt('a', 1, true)
+console.log(s1.print()) // ['a', 1, true]
 
-s1 = push("'a'", s1)
-s1 = push(1, s1)
-s1 = push(true, s1)
-console.log(s1.toString()) // ['a', 1, true]
+s1 = push_adt('b', s1)
+s1 = push_adt(2, s1)
+s1 = push_adt(false, s1)
+console.log(s1.print()) // ['a', 1, true, 'b', 2, false]
 
-const [x, rest] = pop(s1)
-console.log(x) // true
-console.log(s1.toString()) // ['a', 1, true] | note that the original Stack is unchanged!
-console.log(rest.toString()) // ['a', 1]
+const [popped_adt, rest_adt] = pop_adt(s1)
+console.log(popped_adt) // false
+console.log(rest_adt.print()) // ['a', 1, true, 'b', 2]
+console.log(s1.print()) // ['a', 1, true, 'b', 2, false]
+// note that the original Stack is unchanged.
+s1 = stack_adt(...rest_adt.print()) // here, we pass the rest_adt into a new Stack
+console.log(s1.print()) // ['a', 1, true, 'b', 2]
+// _now_ we get the new Stack.
 
 // implementing w/ pure functions using JS's Array prototype
-type StackTwo<T> = (...items: T[]) => T[]
+type Stack<T> = (...items: T[]) => T[]
 type Push<T> = (a: T, stack: T[]) => T[]
 type Pop<T> = (stack: T[]) => [T | undefined, T[]]
 
 /** Takes an arbitrary number of items, and returns a new array with those items. */
-const stackTwo: StackTwo<unknown> = (...items) => [...items]
+const stack: Stack<unknown> = (...items) => [...items]
 
-const pushTwo: Push<unknown> = (a, s) => s.concat(a)
-const popTwo: Pop<unknown> = (s) => {
-  const newStack = s.slice(0)
+const push: Push<unknown> = (a, s) => s.concat(a)
+const pop: Pop<unknown> = (s) => {
+  const newStack = [...s] // can use s.slice() too
   const item = newStack.pop()
 
   return [item, newStack]
 }
 
-let s2 = stackTwo('b', 2)
-console.log(s2) // ['b', 2]
-s2 = pushTwo(false, s2)
-console.log(s2) // ['b', 2, false]
-console.log(popTwo(s2)) // [ false, ['b', 2] ]
+console.log('--- Array prototype tests ---')
+let s2 = stack('a', 1, true)
+console.log(s2) // ['a', 1, true]
+
+s2 = push('b', s2)
+s2 = push(2, s2)
+s2 = push(false, s2)
+console.log(s2) // ['a', 1, true, 'b', 2, false]
+
+const [popped, rest] = pop(s2)
+console.log(popped) // false
+console.log(rest) // ['a', 1, true, 'b', 2]
+console.log(s2) // ['a', 1, true, 'b', 2, false] (unchanged)
+s2 = stack(...rest) // pass the rest into a new Stack
+console.log(s2) // ['a', 1, true, 'b', 2] (new Stack)
